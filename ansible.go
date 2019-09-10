6d589271a774b52f6efdf078f411c9e8cc03717d
Nathaniel Felsen <nfelsen@tesla.com>
2019-09-10 12:37:10 -0700

    Moving the for loop of target bellow the execution of the ansible-playbook --list-hosts execution

--- ansible.go
+++ ansible.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net"
 	"os"
 	"path/filepath"
@@ -25,19 +26,19 @@ type AnsibleConfig struct {
 }
 
 // CreateAnsibleTargetsList generates and returns an array of possible targets
-// The possible targets are ip addresse from the host interfaces or the hostname itself
-func CreateAnsibleTargetsList() []string {
+// The possible targets are ip addresses from the host interfaces or the hostname itself
+func CreateAnsibleTargetsList() ([]string, error) {
 	var inventoryTargets = []string{}
 
 	// extract all ip addresses except the loopback one and add the hostname as a last fallback mechanism
 	ifaces, err := net.Interfaces()
 	if err != nil {
-		logrus.Fatal("Unable to get the network interfaces")
+		return []string{}, errors.Wrap(err, "Unable to get the network interfaces")
 	}
 	for _, i := range ifaces {
 		addrs, err := i.Addrs()
 		if err != nil {
-			logrus.Fatal("Unable to extract the ip addresses")
+			return []string{}, errors.Wrap(err, fmt.Sprintf("Unable to extract the ip addresses from %s", i.Name))
 		}
 		for _, addr := range addrs {
 			var ip net.IP
@@ -54,11 +55,11 @@ func CreateAnsibleTargetsList() []string {
 		}
 	}
 	inventoryTargets = append(inventoryTargets, hostname)
-	return inventoryTargets
+	return inventoryTargets, nil
 }
 
 // FindInventoryForHost calls CreateAnsibleTargetsList to get a list of possible targets
-// and returns the traget and inventory where the target was found.
+// and returns the inventory and target that was found in that inventory.
 // It stops on the first target match meaning that it assumes that hosts are
 // defined by only 1 type of target like their eth0 ip address or hostname
 // If the host was not found, it returns an error.
@@ -68,37 +69,38 @@ func CreateAnsibleTargetsList() []string {
 //
 // The given hostname should be the full name that appears in the Ansible inventories.
 func (a AnsibleConfig) FindInventoryForHost() (string, string, error) {
-	targets := CreateAnsibleTargetsList()
-	for _, target := range targets {
-		for _, item := range a.InventoryList {
-			inv := filepath.Join(a.Cwd, item)
-			_, err := os.Stat(inv)
-			if err != nil {
-				if os.IsNotExist(err) {
-					return "", "", errors.Wrapf(err, "unable to find inventory: %s", item)
-				}
-
-				return "", "", err
+	targets, err := CreateAnsibleTargetsList()
+	if err != nil {
+		return "", "", errors.Wrap(err, "Failed to perform the CreateAnsibleTargetsList command")
+	}
+	for _, item := range a.InventoryList {
+		inv := filepath.Join(a.Cwd, item)
+		_, err := os.Stat(inv)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return "", "", errors.Wrapf(err, "unable to find inventory: %s", item)
 			}
 
-			vCmd := VenvCommand{
-				Config: a.VenvConfig,
-				Binary: "ansible-playbook",
-				Args:   []string{viper.GetString("ansible-playbook"), "-i", inv, "--list-hosts"},
-				Cwd:    a.Cwd,
-			}
+			return "", "", err
+		}
 
-			output, _, err := vCmd.Run()
-			if err != nil {
-				logrus.Debugln("Ansible inventory output:", output)
-				return "", "", errors.Wrap(err, "unable to list hosts for "+item)
-			}
+		vCmd := VenvCommand{
+			Config: a.VenvConfig,
+			Binary: "ansible-playbook",
+			Args:   []string{viper.GetString("ansible-playbook"), "-i", inv, "--list-hosts"},
+			Cwd:    a.Cwd,
+		}
 
+		output, _, err := vCmd.Run()
+		if err != nil {
+			logrus.Debugln("Ansible inventory output:", output)
+			return "", "", errors.Wrap(err, "unable to list hosts for "+item)
+		}
+		for _, target := range targets {
 			if strings.Contains(output, target) {
 				logrus.Debug("Found ", target, " in inventory ", inv)
 				return inv, target, nil
 			}
-
 			logrus.Debug("Did not find ", target, " in inventory ", inv)
 		}
 	}
