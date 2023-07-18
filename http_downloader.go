fb9ddd336b00a5240eef0810d7213d3d77e9eff6
George H. Sun <shk3@users.noreply.github.com>
2023-07-18 17:34:14 +0800

    Handle 404 not found of retrieving md5sum using status code. (#33)
    http.Client.Do never returns error for non-2xx status code according to the doc. Therefore we must rely on the status code to check for 404 not found. We will fix the error handling in later PRs.
    
    This PR also makes the empty md5sum return explicitly and uses a quick-return pattern. https://github.com/golang/go/wiki/CodeReviewComments#indent-error-flow

--- http_downloader.go
+++ http_downloader.go
@@ -76,9 +76,12 @@ func (downloader httpDownloader) RemoteChecksum(remotePath string) (string, erro
 		req.SetBasicAuth(downloader.username, downloader.password)
 	}
 
-	resp, err := client.Do(req)
-	if err != nil {
+	// A non-2xx status code does not cause an error. https://pkg.go.dev/net/http#Client.Do
+	resp, _ := client.Do(req)
+	// Ignore the checksum if it's not found, as assumed by the caller of this function.
+	if resp.StatusCode == http.StatusNotFound {
 		logrus.Debugf("MD5 sum not found at: %s", hashRemotePath)
+		return "", nil
 	}
 
 	logrus.Debugf("Found MD5 sum at: %s", hashRemotePath)
