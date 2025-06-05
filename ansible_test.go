1c11d3957a88792ea2d41a6200fb7393d92196ff
Ashritha Goramane <44136484+ashrithagoramane@users.noreply.github.com>
2025-06-05 03:10:34 -0700

    Logging and exit code fixes (#54)
    1. When debug=true for logging, ansible_puller fails to report stats to
    prometheus
    2. LastExitCode prom_metric is not set for all the errors like when
    inventory pull fails/remote download fails
    
    ---------
    
    Co-authored-by: Ashritha Goramane <agoramane@tesla.com>

--- ansible_test.go
+++ ansible_test.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,4 +29,49 @@ func TestHostnameLookup(t *testing.T) {
 		found = found || matched
 	}
 	assert.True(t, found, "one of the targets should be an ip address")
+}
+
+func TestParsePlayRecap(t *testing.T) {
+	target := `testhostname`
+	var ansibleOutput AnsibleRunOutput
+	ansibleOutput.CommandOutput.Stdout = `
+		PLAY RECAP *********************************************************************
+		testhostname                  : ok=120  changed=5    unreachable=1    failed=2    skipped=184  rescued=3    ignored=4
+		testhostname2                  : ok=125  changed=3    unreachable=2    failed=5    skipped=183  rescued=1    ignored=2
+	`
+
+	ansibleOutput, err := parsePlayRecap(ansibleOutput)
+	assert.Nil(t, err)
+
+	expectedStats := map[string]AnsibleNodeStatus{
+		"testhostname": {
+			Ok:          120,
+			Changed:     5,
+			Unreachable: 1,
+			Failures:    2,
+			Skipped:     184,
+		},
+		"testhostname2": {
+			Ok:          125,
+			Changed:     3,
+			Unreachable: 2,
+			Failures:    5,
+			Skipped:     183,
+		},
+	}
+
+	assert.Contains(t, ansibleOutput.Stats, target, "testhostname should be a key in the Stats map")
+	assert.Empty(t, cmp.Diff(expectedStats, ansibleOutput.Stats))
+}
+
+func TestParsePlayRecapFailed(t *testing.T) {
+	var ansibleOutput AnsibleRunOutput
+	ansibleOutput.CommandOutput.Stdout = `
+		PLAY RECAP *********************************************************************
+		testhostname                  : ok:120  changed:5    unreachable:1    failed:2    skipped:184  rescued:3    ignored:4
+	`
+
+	ansibleOutput, err := parsePlayRecap(ansibleOutput)
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, "recap line doesn't match the expected format")
 }
